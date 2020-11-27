b4ab9baea15c983c4343fd7e99c673192bfc236b
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-11-27 19:17:14 +0000

    add config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 8,
+    },
+    extends: 'google',
+    rules: {
+        'max-len': [
+            'error',
+            {
+                ignoreComments: true,
+                code: 120,
+            },
+        ],
+    },
+};
